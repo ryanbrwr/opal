e0fa8e9f72835e95e459bd1fe187ac4a25cde455
einstein8612 <128143@student.hetbaken.nl>
2020-10-11 22:30:19 +0200

    Update app.js
    Prepare command handler

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 // DEPENDENCIES
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-require('dotenv').config()
+
+const fs = require('fs');
+
+require('dotenv').config();
 //
- 
-const ebay = new (require('./features/ebay'))();
-const email = new (require('./features/email'))();
-const address = new (require('./features/address'))();
-const control = new (require('./features/control'))();
-const fee = new (require('./features/fee'))();
-const help = new (require('./features/help'))();
-const curr = new (require('./features/curr'))
-const downloads = new (require('./features/downloads'))
-const ping = new (require('./features/ping'))
-const shoe = new (require('./features/shoe'))
-const poll = new (require('./features/poll'))
-const botbroker = new (require('./features/botbroker'))
-const embed = new (require('./features/embed'))
-const reminder = new (require('./features/reminder'))
-const giveaway = new (require('./features/giveaway'))
-const delay = new (require('./features/delay'))
-const snowflake = new (require('./features/snowflake'))
-const geocoder = new (require('./features/geocoder'))
-const supcommunity = new (require('./features/supcommunity'))
+
+// Command handler setup
+const PREFIX = '!';
+
+bot.commands = new Discord.Collection();
+
+const featureFiles = fs.readdirSync('./features').filter(file => file.endsWith('.js'));
+
+for (const file of featureFiles) {
+	const feature = require(`./features/${file}`);
+	client.commands.set(feature.name, feature);
+}
  
 bot.on('message', (msg) => {
     let sum = 0;
@@ -30,69 +25,27 @@ bot.on('message', (msg) => {
         sum += guild.members.size
     })
     bot.user.setActivity(`Serving ${sum} people in ${bot.guilds.size} servers`)
+ 
+    // Sender is a bot and should not be served
     if(msg.author.bot) return;
-
-    if (msg.content.startsWith(`!help`)) {
-        help.help_message(msg);
-    }
-    else if (msg.content.startsWith(`!fee`)) {
-       fee.calculate_fee(msg);
-    }
-    else if (msg.content.startsWith(`!email`)) {
-        email.generate(msg);
-    }
-    else if (msg.content.startsWith(`!address`)) {
-        address.generate(msg);
-    }
-    else if (msg.content.startsWith(`!ebay`)) {
-        ebay.generate(msg);
-    }
-    else if (msg.content.startsWith(`!downloads`)) {
-        downloads.list(msg)
-    }
-    else if (msg.content.startsWith(`!delay`)) {
-        delay.send(msg)
-    }
-    else if (msg.content.startsWith(`!snowflake`)) {
-        snowflake.convert(msg)
-    }
-    else if (msg.content.startsWith(`!convert`)) {
-        curr.convert(msg)
-    }
-    else if (msg.content.startsWith(`!spoof`)) {
-        geocoder.convert(msg)
-    }
-    else if (msg.content.startsWith(`!ping`)) {
-        ping.send(msg)
-    }
-    else if (msg.content.startsWith(`!botbroker`)) {
-        botbroker.scrape(msg)
-    }
-    else if (msg.content.startsWith(`!shoe`)) {
-        shoe.convert(msg)
-    }
-    else if (msg.content.startsWith(`!sellout`)) {
-        supcommunity.sellout(msg)
-    }
-    else if (msg.content.startsWith(`!droplist`)) {
-        supcommunity.droplist(msg)
-    }
-    if (msg.member && msg.member.hasPermission("ADMINISTRATOR")) {
-        if (msg.content.startsWith(`!poll`)) {
-            poll.send(msg)
-        }
-        else if (msg.content.startsWith(`!archive`)) {
-            control.archive(msg)
-        }
-        else if (msg.content.startsWith(`!giveaway`)) {
-            giveaway.send(msg)
-        }
-        else if (msg.content.startsWith(`!embed`)) {
-            embed.send(msg)
-        }
-        else if (msg.content.startsWith(`!reminder`)) {
-            reminder.send(msg)
-        }
+ 
+    // Sender is the bot itself and should not be served
+    if (msg.author.id === bot.user.id) return;
+ 
+    const args = msg.split(' ');
+    const command = args.shift();
+ 
+    // Check if command starts with prefix, if the prefix isn't present this message should be ignored
+    if (!command.startsWith(PREFIX)) return;
+ 
+    for (const command of bot.commands) {
+       if (command.name.equalsIgnoreCase(command)) return;   
+  
+       const isAdmin = msg.member && msg.member.hasPermission("ADMINISTRATOR");
+       // If user isn't admin but the command requires it return
+       if (command.admin && !isAdmin) return; 
+       
+       command.execute(msg)
     }
 });
 
