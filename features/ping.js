c200db4d40fa02b5235889ebaf695ed4ad31d587
einstein8612 <128143@student.hetbaken.nl>
2020-10-13 01:02:08 +0200

    Droplist + sellout fixes and an old change I made to the ping command (#17)
    * Ping command update
    
    * Update supcommunity_droplist.js
    
    Fix + getting the next thursday serverside
    
    * Update supcommunity_sellout.js
    
    Optimization
    
    * Update supcommunity_droplist.js
    
    Slight message change

--- features/ping.js
+++ features/ping.js
@@ -4,10 +4,15 @@ module.exports = {
   name: 'ping',
   description: 'This command will display the latency between Discord and our servers',
   async execute(msg) {
-    const embed = new Discord.RichEmbed()
-    embed.setTitle("Ping")
-    embed.setColor('#36393F');
-    embed.setDescription(":ping_pong: Pong!");
-    msg.channel.send(embed)
+    msg.channel.send('Pinging...').then(message => {
+      const embed = new Discord.RichEmbed()
+      embed.setTitle("Ping")
+      embed.setColor('#36393F');
+      embed.setDescription(
+        `:ping_pong: Pong!
+        API: ${message.createdTimestamp - msg.createdTimestamp}ms`
+      );
+      message.edit(embed)
+    })
   }
 }
--- features/supcommunity_droplist.js
+++ features/supcommunity_droplist.js
@@ -1,35 +1,50 @@
+const Discord = require('discord.js')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const re = /[^0-9\/£\$]/g
+
 module.exports = {
-    name: 'droplist',
-    description: 'This command will',
-    async execute(msg) {
-        if (msg.content.split(" ").length != 1) {
-            const embed = new Discord.RichEmbed()
-            embed.setTitle("Error")
-            embed.setDescription("Command is missing one or more arguments")
-            embed.setColor("#36393F")
-            msg.channel.send(embed)
-          }
-          else {
-            const base = 'https://www.supremecommunity.com'
-            const html1 = await axios.get(`https://www.supremecommunity.com/season/fall-winter2020/droplists/`);
-            let $ = await cheerio.load(html1.data);
-            let latest_week = $('a[class="block"]').attr("href")
-            const html2 = await axios.get(`${base}${latest_week}`);
-            $ = await cheerio.load(html2.data);
-            this.items = []
-            this.complete = []
-            $('div[class="card-details"]').each((a, b) => {
-              this.items.push($(b).attr("data-itemname"))
-            })
-            $('span[class="label-price"]').each((a, b) => {
-              this.complete.push(`${this.items[a]} | ${$(b).text().trim()}`)
-            })
-            this.complete = this.complete.join('\n')
-            const embed = new Discord.RichEmbed()
-            embed.setTitle("Latest Supreme Droplist")
-            embed.setDescription(this.complete)
-            embed.setColor("#36393F")
-            msg.channel.send(embed)
-          }
+  name: 'droplist',
+  description: 'This command will',
+  async execute(msg) {
+    const args = msg.content.split(' ')
+    if (args.length != 1) {
+      const embed = new Discord.RichEmbed()
+      embed.setTitle("Error")
+      embed.setDescription("Command is missing one or more arguments")
+      embed.setColor("#36393F")
+      msg.channel.send(embed)
+      return
     }
+
+    const base = 'https://www.supremecommunity.com/season/fall-winter2020/droplist/'
+    const date = new Date()
+    const nextThursday = date.getDate() + (7 - date.getDay() + 4) % 7
+    const latest_week = `${date.getFullYear()}-${(date.getMonth() + 1).length === 1 ? '0' + date.getMonth() + 1 : date.getMonth() + 1}-${nextThursday.length === 1 ? '0' + nextThursday : nextThursday}`
+    const html = await axios.get(base + latest_week);
+    $ = await cheerio.load(html.data);
+    const items = []
+    $('div[class="card-details"]').each((index, b) => {
+      const card = $(b)
+      const name = card.attr('data-itemname')
+      const price = card.find('span[class="label-price"]').text().replace(re,"")
+      items.push({
+        name,
+        price
+      })
+    })
+
+    let droplistString = ''
+    for (const item of items) {
+      if (droplistString !== '') droplistString += '\n'
+      droplistString += `${item.name}: ${item.price ? item.price : '**price not known yet**'}`
+    }
+
+    const embed = new Discord.RichEmbed()
+    embed.setTitle("Latest Supreme Droplist")
+    embed.setDescription(droplistString)
+    embed.setColor("#36393F")
+    msg.channel.send(embed)
+  }
 }
