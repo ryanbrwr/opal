bb2fc39752f30f838583e503030f72f83859fc73
Ryan Brewer <ryanbrew13@gmail.com>
2020-10-13 00:51:35 -0400

    added time command

--- features/time.js
+++ features/time.js
@@ -1,16 +1,16 @@
-const https = require('https');
+const axios = require("axios")
 const Discord = require("discord.js")
 
-function format(date, offset) {
-    var hours = date.getHours() + offset;
+function format(date) {
+    var hours = date.getHours();
     var minutes = date.getMinutes();
-    var ampm = hours > 24 ? 'am' : 'pm';
+    var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
-}
+  }
 
 let timezones = ['America/Los_Angeles', 'America/New_York', "Europe/London", "Europe/Berlin", "Australia/Sydney", "Asia/Hong_Kong", "Asia/Tokyo"]
 
@@ -19,34 +19,18 @@ module.exports = {
     description: 'This command will list out timezones',
     async execute(msg) {
         const embed = new Discord.RichEmbed()
-        embed.setTitle("Timezones")
-        timezones.forEach(async (timezone) => {
-            https.get(`https://worldtimeapi.org/api/timezone/${timezone}`, async (resp) => {
-                let data = ''
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-            
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                    console.log("HI")
-                    data = JSON.parse(data);
-                    let { datetime, utc_offset } = data
-                    let date = new Date(datetime);
-                    let offset = parseInt(utc_offset.slice(0,3)) + 4;
-                    let time = format(date, offset)
-                    let area = timezone.split('/')[1]
-                    embed.addField(area, time);
-                });
-            
-            }).on("error", (err) => {
-                const error = new Discord.RichEmbed()
-                error.setTitle("Error");
-                error.setDescription("Error getting current time")
-                msg.channel.send(error)
-                return
-            });
-        })
+        embed.setTitle("World Clock")
+        await Promise.all(timezones.map(async (timezone) => {
+            const response = await axios.get(`https://worldtimeapi.org/api/timezone/${timezone}`)
+            let { data } = response
+            let { datetime, } = data
+            datetime = datetime.slice(0, datetime.length -6)
+            let date = new Date(datetime);
+            let time = format(date, 0)
+            let area = timezone.split('/')[1]
+            embed.addField(area.replace('_', ' '), time);
+        }))
+
         embed.setColor("#36393F")
         embed.setTimestamp();
         embed.addField("Invite Opal", "https://bit.ly/invite-opal", true)
