9ffe7875e77d827bd46dd4f3157c693e641647e3
Ryan Brewer <ryanbrew13@gmail.com>
2020-10-19 00:33:47 -0400

    removed ebay.js and added branding to ping

--- features/ping.js
+++ features/ping.js
@@ -9,9 +9,12 @@ module.exports = {
       embed.setTitle("Ping")
       embed.setColor('#36393F');
       embed.setDescription(
-        `:ping_pong: Pong!
-        API: ${message.createdTimestamp - msg.createdTimestamp}ms`
+        `:ping_pong: Pong! ${message.createdTimestamp - msg.createdTimestamp}ms`
       );
+      embed.setColor("#36393F")
+      embed.setTimestamp();
+      embed.addField("\u200b", "[Invite Opal](https://bit.ly/opal-invite) | [Join Server](https://bit.ly/opal-join-discord) | [Twitter](https://twitter.com/OpalSource)", true)
+      embed.setFooter("opal.io", "https://i.ibb.co/BG79PK2/opallogo.png")
       message.edit(embed)
     })
   }
